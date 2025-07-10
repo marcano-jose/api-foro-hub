1fbb8c806217aa850d2f1746167e4d11f7bdbf07
marcano-jose <marcano_juan@proton.me>
2025-07-10 08:54:55 -0400

    Ajuste sintaxis del script migration V1

--- src/main/java/hub/foro/api/topico/TopicoRepository.java
+++ src/main/java/hub/foro/api/topico/TopicoRepository.java
@@ -1,9 +1,8 @@
 package hub.foro.api.topico;
 
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-    boolean existsByTituloAndMensaje(@NotBlank String titulo, @NotBlank String mensaje);
+    boolean existsByTituloAndMensaje(String titulo, String mensaje);
 }
--- src/main/resources/db/migration/V1__create-table-forohub_api.sql
+++ src/main/resources/db/migration/V1__create-table-forohub_api.sql
@@ -1,9 +1,19 @@
-CREATE TABLE topicos (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    mensaje TEXT NOT NULL,
-    fecha_creacion TIMESTAMP NOT NULL,
-    status BOOLEAN NOT NULL,
-    autor VARCHAR(20) NOT NULL,
-    curso VARCHAR(20) NOT NULL
-);
+-- V1__create-table-topicos.sql
+-- Descripción: Script de migración SQL para la creación de la tabla `topicos` en una base de datos MySQL.
+-- Esta tabla está diseñada para la gestión de tópicos de la API `api-foro-hub` y cuenta con una
+-- restricción de unicidad compuesta en los campos `titulo` y `mensaje`.
+
+CREATE TABLE `topicos` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `titulo` varchar(255) NOT NULL,
+    `mensaje` text NOT NULL,
+    `fecha_creacion` timestamp NOT NULL,
+    `status` tinyint(1) NOT NULL, -- Representa un valor booleano (1 para activo, 0 para inactivo)
+    `autor` varchar(20) NOT NULL,
+    `curso` varchar(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    -- Restricción única compuesta para título y mensaje, usando un prefijo de 255 caracteres para 'mensaje'
+    UNIQUE KEY `UQ_titulo_mensaje` (`titulo`,`mensaje`(255))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Fin del script de creación de tabla 'topicos'
